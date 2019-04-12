d7bdf3a6ad8298bdba2b9dc3e092357e7b0b8535
Gabriel Lopes <gabrielopesrodrigues@gmail.com>
2019-04-11 21:56:01 -0300

    Creation of FileController and association of file with box.

--- src/controllers/BoxController.js
+++ src/controllers/BoxController.js
@@ -6,6 +6,13 @@ class BoxController {
 
         return res.json(box);
     }
+
+    async show(req, res) {
+        const box = await Box.findById(req.params.id);
+        //const box = await Box.findById(req.params.id).populate("files");
+
+        return res.json(box);
+    }
 }
 
 module.exports = new BoxController();
