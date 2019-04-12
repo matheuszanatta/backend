61aee2bb6019a310e11d9d8c20c3cf137d9a4cc8
Gabriel Lopes <gabrielopesrodrigues@gmail.com>
2019-04-11 22:46:36 -0300

    Bug fix.

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ mongoose.connect('mongodb+srv://database:mtgdatabase@cluster0-bhc5y.mongodb.net/
     useNewUrlParser: true
 });
 
-app.user((req, res) => {
+app.use((req, res) => {
     req.io = io;
 
     return next();
