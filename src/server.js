8b6f6a715d08fb35d70fae47267e92753f873cb5
Gabriel Lopes <gabrielopesrodrigues@gmail.com>
2019-04-11 22:36:09 -0300

    Added realtime function to the server.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on("connection", socket => {
+    socket.on('connectRoom', box => {
+        socket.join(box);
+    })
+});
+
 mongoose.connect('mongodb+srv://database:mtgdatabase@cluster0-bhc5y.mongodb.net/teste?retryWrites=true', {
     useNewUrlParser: true
+});
+
+app.user((req, res) => {
+    req.io = io;
+
+    return next();
 });
 
 app.use(express.json());
@@ -14,4 +32,4 @@ app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
